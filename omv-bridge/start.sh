ffd910468d9ce31d870dd02447303d12f8fc763f
smurfy7625-commits <smurfy7625@gmail.com>
2026-09-21 22:57:18 -0400

    Delete omv-bridge directory

--- omv-bridge/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-export KYBER_BRIDGE_SOCKET="${KYBER_BRIDGE_SOCKET:-/run/kyber-omv/bridge.sock}"
-
-echo "Starting Kyber Core OMV bridge on Unix socket:"
-echo "$KYBER_BRIDGE_SOCKET"
-
-exec /usr/bin/python3 /opt/kyber-omv-bridge/bridge.py
